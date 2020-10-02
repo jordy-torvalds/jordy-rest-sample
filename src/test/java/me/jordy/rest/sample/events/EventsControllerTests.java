f67768853ef1169053d426e1bac967d8faa59941
jordy-torvalds <jordy-torvalds@kakao.com>
2020-10-02 23:19:45 +0900

    [수정-리팩토링] 상속을 활용한 컨트롤러 어노태이션 및 Autowired 구문 공통 처리

--- src/test/java/me/jordy/rest/sample/events/EventsControllerTests.java
+++ src/test/java/me/jordy/rest/sample/events/EventsControllerTests.java
@@ -2,6 +2,7 @@ package me.jordy.rest.sample.events;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.jordy.rest.sample.common.BaseControllerTest;
 import me.jordy.rest.sample.common.RestDocsConfiguration;
 import me.jordy.rest.sample.common.TestDescription;
 import org.hamcrest.Matchers;
@@ -34,36 +35,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-/* Junit 4 를 기준으로 작성된 테스트 */
-@RunWith(SpringRunner.class)
-/**
- * @WebMvcTest는 컨트롤러 레이어에 슬라이싱 테스트를 적용하는 어노태이션.
- * 그래서  Repository는 빈 등록이 되지 않음.
- * 만약 테스트 중 실제 DB에 CRUD 하는 것이 필요할 경우 아래와 같이 어노태이션 교체가 필요
- * - @SpringBootTest           <-- 통합테스트 시 사용되는 어노태이션. 기본 웹 환경 값이 Mock으로 되어 있습니다.
- * - @AutoConfigureMockMvc     <-- MockMvc를 자동설정 해주는 어노태이션
- */
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class) /* 설정 클래스에 설정된 정보를 적용하기 위해 임의로 임포트. 그냥 해서는 적용 안 됨*/
-@ActiveProfiles("test")
-public class  EventsControllerTests {
-
-    /**
-     * - 웹 서버를 띄우지 않고도 스프링 MVC (DispatcherServlet) 가 요청을 처리하는 과정을 확인할 수 있어
-     *   컨트롤러 테스트용으로 자주 쓰임.
-     * - 단위테스트라고 보기는 힘듬.(너무 많은 라이브러리가 사용됨
-     */
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Autowired
-    ModelMapper modelMapper;
+public class  EventsControllerTests extends BaseControllerTest {
 
     @Autowired
     EventRepository eventRepository;
